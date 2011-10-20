32b7a381f11c696f12d2d12094fe4fe85c4052d0
Daniel Huelsman <daniel.huelsman@gmail.com>
2011-10-20 00:47:45 -0400

    Updated for 2nd Milestone. Ready for submission.

--- transmitter.c
+++ transmitter.c
@@ -21,6 +21,8 @@
 
 
 void Convert2Bin(int, char[]);
+void HammingEncode (char[],char[]);
+void CRCEncode (char[],char[]);
 
 int main(int argc, char** argv) {
      char userFile[256], dir[256];
@@ -31,11 +33,11 @@ int main(int argc, char** argv) {
      char*  binBuffer;      // Buffer of the file converted to binary.
      char buffsyn[8] = "00010110"; // Sync Character in binary.
      char* frame;        // What its name says. 1 Frame 
-     char cBuffer[8],  ext[256];          // Input character in binary
+     char cBuffer[8], ceBuffer[13],  ext[256];          // Input character in binary
      char* dataSbin;     // Size of the data block in binary.
      char* extBin;
      int buffin;         // Input buffer
-
+     char encoding[5];       // Stores encoding selection
      /*
       * counter i, Buffer Length, Number of Frames, Data Size.
       */
@@ -44,6 +46,11 @@ int main(int argc, char** argv) {
      while(1){
     /** Get the file location, file name and file extension */
         printf("Enter 'exit' for dir to quit. \n");
+        printf("Enter 1 for Hamming Encoding \nEnter 2 For CRC\n");
+        fgets(encoding, sizeof(encoding), stdin);
+        if(strncmp(encoding,"exit",4)==0){
+            break;
+        }
         printf("Enter directory of the file: \n Ex: /home/user/\n");
         fgets(dir,sizeof(dir), stdin);
         if(strncmp(dir,"exit",4)==0){
@@ -85,7 +92,7 @@ int main(int argc, char** argv) {
         fseek (fins , 0 , SEEK_END);
         fSize = ftell (fins);
         rewind (fins);      
-        blen = fSize*8;
+        blen = fSize*13;
         
         binBuffer = (char*) malloc (sizeof(char)*(blen));
         if (binBuffer == NULL) {fputs ("Memory error",stderr); exit (2);}
@@ -93,39 +100,64 @@ int main(int argc, char** argv) {
         /*Read the file into a Buffer*/
         while((buffin=fgetc(fins))!=EOF){
             Convert2Bin(buffin, cBuffer);
-            strncat(binBuffer, cBuffer, 8);
+            if(encoding[0]=='1'){
+                HammingEncode(cBuffer,ceBuffer);
+            }else if(encoding[0]=='2'){
+                CRCEncode(cBuffer,ceBuffer);
+            }
+            strncat(binBuffer, ceBuffer, 13);
         }
         
-        extBin = (char*) malloc (sizeof(char)*(32));
+        extBin = (char*) malloc (sizeof(char)*(52));
         if (extBin == NULL) {fputs ("Memory error",stderr); exit (2);}
         
         /*Convert extension into binary*/
         for(i=0;i<4;i++){
             Convert2Bin(ext[i],cBuffer);
-            strncat(extBin, cBuffer, 8);
+            if(encoding[0]=='1'){
+                HammingEncode(cBuffer,ceBuffer);
+            }else if(encoding[0]=='2'){
+                CRCEncode(cBuffer,ceBuffer);
+            }
+            strncat(extBin, ceBuffer, 13);
         }
         
-        frame = (char*) malloc (sizeof(char)*(88));
+        frame = (char*) malloc (sizeof(char)*(143));
           if (frame == NULL) {fputs ("Memory error",stderr); exit (2);}
         
-        dataSbin = (char*) malloc (sizeof(char)*(8));
+        dataSbin = (char*) malloc (sizeof(char)*(13));
           if (dataSbin == NULL) {fputs ("Memory error",stderr); exit (2);}
         
         /*Pack the extension into the first frame*/
-        strncpy(frame, buffsyn, 8);        
-        Convert2Bin(32,dataSbin);        
-        strncat(frame, dataSbin, 8);
-        strncat(frame, extBin, 32);
-        strncat(frame, buffsyn, 8);
+        if(encoding[0]=='1'){
+                HammingEncode(buffsyn,ceBuffer);
+            }else if(encoding[0]=='2'){
+                CRCEncode(buffsyn,ceBuffer);
+            }
+        strncpy(frame, ceBuffer, 13);        
+        Convert2Bin(52,dataSbin);
+        if(encoding[0]=='1'){
+                HammingEncode(dataSbin,ceBuffer);
+            }else if(encoding[0]=='2'){
+                CRCEncode(dataSbin,ceBuffer);
+            }
+        strncat(frame, ceBuffer,13 );
+        strncat(frame, extBin, 52);
+        if(encoding[0]=='1'){
+                HammingEncode(buffsyn,ceBuffer);
+            }else if(encoding[0]=='2'){
+                CRCEncode(buffsyn,ceBuffer);
+            }
+        strncat(frame, ceBuffer, 13);
         
         /*Write the first frame to the file*/
-        fwrite(frame,1,56,fos);
+        fwrite(frame,1,91,fos);
 
         /*Find the number of Frames*/
-        if(blen%64==0){
-           framen = blen/64;
+        if(blen%104==0){
+           framen = blen/104;
         }else{
-           framen = blen/64 + 1;
+           framen = blen/104 + 1;
         }
         
         /*
@@ -133,35 +165,49 @@ int main(int argc, char** argv) {
          * as it is created
          */
         for(i= 0;i<framen;i++){
-          frame = (char*) malloc (sizeof(char)*(88));
+          frame = (char*) malloc (sizeof(char)*(143));
           if (frame == NULL) {fputs ("Memory error",stderr); exit (2);}
           
-          dataSbin = (char*) malloc (sizeof(char)*(8));
+          dataSbin = (char*) malloc (sizeof(char)*(13));
           if (dataSbin == NULL) {fputs ("Memory error",stderr); exit (2);}
           
-          strncpy(frame, buffsyn,8);
+          if(encoding[0]=='1'){
+                HammingEncode(buffsyn,ceBuffer);
+            }else if(encoding[0]=='2'){
+                CRCEncode(buffsyn,ceBuffer);
+            }
+          strncpy(frame, ceBuffer,13);
           
           /*Figure out if it is a full 64 bits or not*/
-          if (i==framen-1 && blen%64!=0){
-              dataSize = blen%64;
+          if (i==framen-1 && blen%104!=0){
+              dataSize = blen%104;
           }else{
-              dataSize = 64;
+              dataSize = 104;
           }
           
-          Convert2Bin(dataSize, dataSbin);         
-          strncat(frame, dataSbin, 8);
-          strncat(frame, binBuffer + i*64,dataSize);
-          strncat(frame, buffsyn, 8);
+          Convert2Bin(dataSize, dataSbin); 
+          if(encoding[0]=='1'){
+                HammingEncode(dataSbin,ceBuffer);
+            }else if(encoding[0]=='2'){
+                CRCEncode(dataSbin,ceBuffer);
+            }
+          strncat(frame, ceBuffer, 13);
+          strncat(frame, binBuffer + i*104,dataSize);
+          if(encoding[0]=='1'){
+                HammingEncode(buffsyn,ceBuffer);
+            }else if(encoding[0]=='2'){
+                CRCEncode(buffsyn,ceBuffer);
+            }
+          strncat(frame, ceBuffer, 13);
           
-          fwrite(frame,1,dataSize+24,fos); // Writes to the output file.
+          fwrite(frame,1,dataSize+39,fos); // Writes to the output file.
         }
 
         /*Close file streams and free buffer*/
         fclose(fins);
         fclose(fos);
-        free(binBuffer);
+        //free(binBuffer);
      }
-
 
     return (EXIT_SUCCESS);
 }
@@ -171,11 +217,10 @@ int main(int argc, char** argv) {
  * Converts and integer into a binary string to make it
  * editable by humans and to symbolized the binary data.
  */ 
-void Convert2Bin(int bin, char strOut[12]){
+void Convert2Bin(int bin, char str[8]){
     unsigned int mask;      // used to check each individual bit, 
                             //    unsigned to alleviate sign 
                             //    extension problems
-    char str[8];
     int parity,i;
     parity = 0;
     i=0;
@@ -197,12 +242,10 @@ void Convert2Bin(int bin, char strOut[12]){
         str[0]='1';
     }
     
-    HammingEncode(str,strOut);
 }
 
-void HammingEncode (char str[8],char crstr[12]){
+void HammingEncode (char str[8],char hstr[13]){
     int pOne = 0, pTwo=0, pFour=0, pEight=0;
-    char hstr[13];
     int i, pCount=0;
     
     if (str[0]=='1'){
@@ -264,5 +307,59 @@ void HammingEncode (char str[8],char crstr[12]){
     }
     if(pCount%2!=1){
         hstr[0]='1';
+    }else{
+        hstr[0]='0';
     }
+}
+
+void CRCEncode(char str[8], char crstr[13]){
+int i,j,n=8,g=6,a=13,arr[13],q[13],gen[6];
+// Convert to Int array
+for(i=0;i<n;i++){
+    if(str[i]=='1'){
+        arr[i]=1;
+    }else{
+        arr[i]=0;
+    }
+}
+gen[0]=1;
+gen[1]=0;
+gen[2]=0;
+gen[3]=0;
+gen[4]=1;
+gen[5]=1;
+//Append Trailing Zeros
+for(i=0;i< g-1;++i){
+    arr[n+i]=0;
+}
+//Copy original data into output
+for(i=0;i< n;++i){
+    q[i]= arr[i];
+}
+//Calculate CRC
+for(i=0;i< n;++i){
+    if(arr[i]==0){
+        for(j=i;j< g+i;++j)
+        arr[j] = arr[j]^0;
+    }else{
+        arr[i] = arr[i]^gen[0];
+        arr[i+1]=arr[i+1]^gen[1];
+        arr[i+2]=arr[i+2]^gen[2];
+        arr[i+3]=arr[i+3]^gen[3];
+        arr[i+4]=arr[i+4]^gen[4];
+        arr[i+5]=arr[i+5]^gen[5];
+    }
+}
+//copy CRC into output
+for(i=n;i< a;i++){
+    q[i]=arr[i];
+}
+//Convert back to a string
+for(i=0;i<a;i++){
+    if(q[i]==1){
+        crstr[i]='1';
+    }else{
+        crstr[i]='0';
+    }
+}
 }
